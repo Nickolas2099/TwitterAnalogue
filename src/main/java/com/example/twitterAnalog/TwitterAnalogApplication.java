779d862823c8e1e093f7c22c776aab97d5a0431a
Nickolas2099 <iantoshka2003@mail.ru>
2023-08-02 13:09:56 +0300

    generate application structure

---
+++ src/main/java/com/example/twitterAnalog/TwitterAnalogApplication.java
@@ -0,0 +1,13 @@
+package com.example.twitterAnalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterAnalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterAnalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/twitterAnalog/TwitterAnalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twitterAnalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterAnalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
